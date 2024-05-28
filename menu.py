82aef5d23933286f3b25a05a3ea92e380a434506
smartinezo <47525393+smartinezo@users.noreply.github.com>
2024-05-28 17:19:07 +0200

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,14 +1,1 @@
-import os
-path_dir_base = 'C:/repositorio'
-contenido = os.listdir(path_dir_base)
-
-
-
-directorios = []
-for dir in contenido:
-    path_subdir = path_dir_base + "/" + dir
-    contenidoSubDir =  os.listdir(path_subdir)
-    for subdir in contenidoSubDir:
-        print (subdir)
-
-        
+  
